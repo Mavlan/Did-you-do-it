d0193c6423e520c2d4519faf0e85e62e779cfcbf
Crenshaw Mafia <Doom90119@hotmail.com>
2026-04-08 19:42:08 +0800

    Create core/questie.lua file for Questie integration

---
+++ core/questie.lua
@@ -0,0 +1,16 @@
+-- Questie integration for quest data reading
+-- This file contains functions to read and process quest data.
+
+local Questie = {}
+
+-- Function to initialize Questie
+function Questie:Init()
+    -- Initialization code here
+end
+
+-- Function to read quest data
+function Questie:ReadQuestData(questId)
+    -- Code to read quest data based on questId
+end
+
+return Questie
